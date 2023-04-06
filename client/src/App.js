1222bea264d67451cd941a77610218a2eb2d418d
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-04-05 21:23:58 -0400

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './App.css';
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+            <header className="App-header">
+             <img src={logo} className="App-logo" alt="logo" />
+               <p>
+                 THIS IS A PAGE
+               </p>
+            </header>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
