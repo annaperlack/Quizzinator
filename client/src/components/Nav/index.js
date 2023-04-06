b17a7a65cac110aea506797b6de81a3e002cbb84
unknown <dbp1795@aim.com>
2023-04-06 19:42:55 -0400

    reworked navbar and deleted mainmenu

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,39 +1,53 @@
 import React from "react";
 
-function NavBar({ currentPage, handlePageChange }) {
+function Header({ currentPage, handlePageChange, user }) {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li>
-          <a
-            href="#study"
-            className={`nav-link ${currentPage === "Home" && "active"}`}
-            onClick={() => handlePageChange("Study")}
-          >
-            Study Time
-          </a>
-        </li>
-        <li>
-          <a
-            href="connect"
-            className={`nav-link ${currentPage === "Home" && "active"}`}
-            onClick={() => handlePageChange("Connect")}
-          >
-            Connect
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className={`nav-link ${currentPage === "Home" && "active"}`}
-            onClick={() => handlePageChange("Contact")}
-          >
-            Questions?
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <div className="header-logo">
+        {/* app logo here */}
+      </div>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li>
+            <a
+              href="#study"
+              className={`nav-link ${currentPage === "Study" && "active"}`}
+              onClick={() => handlePageChange("Study")}
+            >
+              Study Time
+            </a>
+          </li>
+          <li>
+            <a
+              href="#connect"
+              className={`nav-link ${currentPage === "Connect" && "active"}`}
+              onClick={() => handlePageChange("Connect")}
+            >
+              Connect
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className={`nav-link ${currentPage === "Contact" && "active"}`}
+              onClick={() => handlePageChange("Contact")}
+            >
+              Questions?
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="header-user">
+        {/* code for profile picture v */}
+        {user && (
+          <>
+            <img src={user.picture} alt={user.name} />
+            <span>{user.name}</span>
+          </>
+        )}
+      </div>
+    </header>
   );
 }
 
-export default NavBar;
+export default Header;
