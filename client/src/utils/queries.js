83c9fc9b6c5ed1cd2dbc2bc25ab8215ef1468381
Anna Perlack <annaperlack@gmail.com>
2023-04-06 19:18:32 -0400

    setup login page and route

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,20 @@
-//get routes 
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($email: String!) {
+    user(email: $email) {
+      _id
+      email
+    }
+  }
+`;
+
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      email
+    }
+  }
+`;
