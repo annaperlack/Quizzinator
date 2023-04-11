71098448695684b66e77b908efce533037281f8e
FDJoss <fdjoss@gmail.com>
2023-04-10 21:55:51 -0400

    set up appbar as navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,61 +1,144 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import Auth from '../../utils/auth';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Auth from '../../utils/auth';
+
+const pages = ['Study', 'Connect', 'Contact'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
 
-function Header({ currentPage, handlePageChange, user }) {
+
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
     window.location.href = '/'
   };
+
   return (
-    <header className="header">
-      <div className="header-logo">
-        {/* app logo here */}
-      </div>
-      <nav className="navbar">
-        <Button
-          variant="contained"
-          href="/study"
-          className={`nav-link ${currentPage === "Study" && "active"}`}
-          to='/study'
-        >
-          Study Time
-        </Button>
-        <Button
-          variant="contained"
-          href="/connect"
-          className={`nav-link ${currentPage === "Connect" && "active"}`}
-          to='/connect'
-        >
-          Connect
-        </Button>
-        <Button
-          variant="contained"
-          href="/contact"
-          className={`nav-link ${currentPage === "Contact" && "active"}`}
-          to='/contact'
-        >
-          Questions?
-        </Button>
-        <Button variant="contained" onClick={logout}>
-          Logout
-        </Button>
-      </nav>
-      <div className="header-user">
-        {/* code for profile picture v */}
-        {user && (
-          <>
-            <img src={user.picture} alt={user.name} />
-            <span>{user.name}</span>
-          </>
-        )}
-      </div>
-    </header>
+    <AppBar position="static">
+      <Container>
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Project3
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Project3
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                href={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+
+              <MenuItem key="Logout" onClick={logout}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
+
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-export default Header;
+export default ResponsiveAppBar;
 
