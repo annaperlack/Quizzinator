ee5373eaf383776ff1f4433e28b5eb156cfe9c53
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-04-13 20:52:16 -0400

    deleted files and added pic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Quiz } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
 
 const resolvers = {
   Query: {
