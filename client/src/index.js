69922bac0e491d67fa7f8f7a221118e4a55fca5b
unknown <dbp1795@aim.com>
2023-04-13 22:16:07 -0400

    heroku stuff

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,24 @@ import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom'
+
+<HashRouter>
+  <App/>
+</HashRouter>
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
+  <HashRouter>
+
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </ThemeProvider>,
+</HashRouter>
 );
 
 reportWebVitals();
